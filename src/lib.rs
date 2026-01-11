70ff5feca73809ff5e355e6791a2f06c1145728f
eiji <eijinakayama21@gmail.com>
2026-01-10 22:25:54 -0500

    deploy again but fixed html and lib issues

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,6 @@ pub struct WebEmulator {
 impl WebEmulator {
     #[wasm_bindgen(constructor)]
     pub fn new(rom_data: Vec<u8>) -> Self {
-        // Use a placeholder name for the save file on web
         let mmu = MMU::new(rom_data, "web_save.sav");
         Self { 
             cpu: CPU::new(mmu),
@@ -29,16 +28,19 @@ impl WebEmulator {
 
     pub fn update_frame(&mut self) {
         let mut cycles_this_frame = 0;
-        // Run roughly 70,224 cycles (one Game Boy frame)
         while cycles_this_frame < 70224 {
             let cycles = self.cpu.step() as u32;
             cycles_this_frame += cycles;
-            // Sync the PPU with the CPU
             self.ppu.tick(&mut self.cpu.bus, cycles as u8);
         }
     }
 
     pub fn get_screen_ptr(&self) -> *const u32 {
         self.ppu.frame_buffer.as_ptr()
+    }
+    
+    // ADD THIS METHOD
+    pub fn get_frame_buffer(&self) -> Vec<u32> {
+        self.ppu.frame_buffer.to_vec()
     }
 }
