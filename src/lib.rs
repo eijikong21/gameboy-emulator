1fb2bdb47d10459e534088f0b9c6ca147c45088d
eiji <eijinakayama21@gmail.com>
2026-01-10 22:54:19 -0500

    working now but with graphic bugs in zelda and still no audio

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 use wasm_bindgen::prelude::*;
 
-// This tells the compiler these files exist
 pub mod cpu;
 pub mod mmu;
 pub mod ppu;
@@ -26,21 +25,37 @@ impl WebEmulator {
         }
     }
 
-    pub fn update_frame(&mut self) {
-        let mut cycles_this_frame = 0;
-        while cycles_this_frame < 70224 {
-            let cycles = self.cpu.step() as u32;
-            cycles_this_frame += cycles;
-            self.ppu.tick(&mut self.cpu.bus, cycles as u8);
+   pub fn update_frame(&mut self) {
+    let mut cycles_this_frame = 0;
+    while cycles_this_frame < 70224 {
+        // Add safety check to prevent infinite loops
+        if cycles_this_frame > 100000 {
+            web_sys::console::log_1(&"Warning: Too many cycles!".into());
+            break;
         }
+        
+        self.cpu.handle_interrupts();  // Make sure interrupts are handled
+        let cycles = self.cpu.step() as u32;
+        cycles_this_frame += cycles;
+        
+        self.cpu.bus.tick(cycles as u8);  // Tick the MMU timers
+        self.ppu.tick(&mut self.cpu.bus, cycles as u8);
     }
+}
 
     pub fn get_screen_ptr(&self) -> *const u32 {
         self.ppu.frame_buffer.as_ptr()
     }
     
-    // ADD THIS METHOD
+    // ADD THESE TWO METHODS IF MISSING:
     pub fn get_frame_buffer(&self) -> Vec<u32> {
         self.ppu.frame_buffer.to_vec()
+    }
+    
+    pub fn set_joypad(&mut self, state: u8) {
+        self.cpu.bus.joypad_state = state;
+        if state != 0xFF {
+            self.cpu.bus.interrupt_flag |= 0x10;
+        }
     }
 }
